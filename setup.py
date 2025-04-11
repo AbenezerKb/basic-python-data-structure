13dffdbf8a93c729f5bf065f5f86c52de539dabb
AbenezerAngamo <aangamo@aimsammi.com>
2025-04-11 00:00:05 +0000

    updated package documentation

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ long_description = (Path(__file__).parent / 'README.md').read_text(encoding='utf
 
 
 setup(
-    name="python_basic_data_structures",
-    version="0.0.2",
+    name="bootcamp_for_python_basic_data_structures",
+    version="0.0.1",
     description="A collection of basic data structures in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Abenezer Angamo",
     author_email="aben.jacob3@gmail.com",
-    url="https://github.com/AbenezerAngamo/data-structures-python",
+    url="https://abenezerkb.github.io/basic-python-data-structure/",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
